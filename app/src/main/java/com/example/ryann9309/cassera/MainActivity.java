a0b3794cc9dd910fe073dcbe50c9439bdb99da87
Ryan E Naccarato <ryann9309@bigfoot.spokane.edu>
2017-01-19 10:24:55 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ryann9309/cassera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryann9309.cassera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
