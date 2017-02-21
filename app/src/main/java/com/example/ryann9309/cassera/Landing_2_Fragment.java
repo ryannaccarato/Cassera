1933d0d8a77eb0092f5367efb81626c25db1de48
Ryan Naccarato <ryannaccarato@yahoo.com>
2017-02-21 10:25:56 -0800

    Added WebView to Landing 2.

--- app/src/main/java/com/example/ryann9309/cassera/Landing_2_Fragment.java
+++ app/src/main/java/com/example/ryann9309/cassera/Landing_2_Fragment.java
@@ -1,16 +1,69 @@
 package com.example.ryann9309.cassera;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Landing_2_Fragment extends Fragment {
+
+    WebView mWebView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_landing_2, container, false);
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mWebView = (WebView)view.findViewById(R.id.webView_Main);
+        startWebView("https://www.stackoverflow.com");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mWebView = null;
+    }
+
+    private void startWebView(String url) {
+        mWebView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onLoadResource (WebView view, String url) {
+                if (progressDialog == null) {
+                    progressDialog = new ProgressDialog(getActivity());
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                try{
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    }
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(url);
+    }
+
 }
